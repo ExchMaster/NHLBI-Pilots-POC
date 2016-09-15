096c005a8353db36e2b3883aace11f630ad3ccab
Jason Ingram <jingram@microsoft.com>
2016-09-15 18:52:28 -0400

    Shell Update

--- Comsol/configstorv8TB.sh
+++ Comsol/configstorv8TB.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+#Update system
+yum update -y
 #Enable X11, deskotp env, etc
-#yum install xorg-x11-xauth xorg-x11-fonts-* xorg-x11-font-utils xorg-x11-fonts-Type1 xorg-x11-apps gdm Xnest -y
+#yum install xorg-x11-xauth xorg-x11-fonts-* xorg-x11-font-utils xorg-x11-fonts-Type1 xorg-x11-apps gdm Xnest xterm -y
 #yum groupinstall 'Server with GUI' 'X Window System' -y
-#lsblk ‒‒noheadings ‒‒output UUID /dev/vg1/lv1
+#lsblk ‒‒noheadings ‒‒output UUID /dev/sdc1
 #http://unix.stackexchange.com/questions/200582/scripteable-gpt-partitions-using-parted
 #https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-linux-classic-attach-disk/
 #Creates Raid 0 array on two disks, /dev/sdc & /dev/sdd
@@ -17,8 +18,7 @@ mkfs.xfs /dev/vg1/lv1 -f
 #Creates mount point and updates fstab so mount point persists during reboots, automatically mounts volumes for current session
 mkdir -p /data
 #Add to fstab
-UUID=$(lsblk --noheadings --output UUID /dev/vg1/lv1)
-echo UUID=$UUID /data                   xfs     defaults        0 0 >> /etc/fstab
+echo /dev/vg1/lv1 /data                   xfs     defaults        0 0 >> /etc/fstab
 #mount volumes
 mount -a
 #Allow everyone access to /data
